679897a299913571fdb3bd0a474afb35bd98a66e
mareathj <jmareath@gmail.com>
2019-09-03 15:54:38 +1000

    Add files via upload

---
+++ FirstProg.java
@@ -0,0 +1,6 @@
+public class FirstProg{
+	public static void main( String[] args ) {
+		System.out.println( "I, Jim Mareath, am determined to learn." );
+		System.out.println( "Today's date is 30/08/19" );
+	}
+}
---
+++ GasolineReciept.java
@@ -0,0 +1,16 @@
+public class GasolineReciept{
+    public static void main( String[] args){
+        System.out.println( "+------------------------+" );
+        System.out.println( "|                        |" );
+        System.out.println( "|      CORNER STORE      |" );
+        System.out.println( "|                        |" );
+        System.out.println( "| 2019-09-02  03:33PM    |" );
+        System.out.println( "|                        |" );
+        System.out.println( "| Gallons:       10.870  |" );
+        System.out.println( "| Price/gallon: $ 2.089  |" );
+        System.out.println( "|                        |" );
+        System.out.println( "| Fuel Total: $ 22.71    |" );
+        System.out.println( "|                        |" );
+        System.out.println( "+------------------------+" );
+    }
+}
